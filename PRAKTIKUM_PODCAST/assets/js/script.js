067ad6ecaa351a2f1057d7783f38b9f14196db1d
MEHMO43 <mehmet.oezer013@stud.fh-dortmund.de>
2023-01-05 20:57:43 +0100

    Praktikum8finished

---
+++ PRAKTIKUM_PODCAST/assets/js/script.js
@@ -0,0 +1,13 @@
+//Geben Sie mittels JavaScript die Breite des Layout-Viewports auf der Konsole aus.
+
+const getViewportWidth = () => window.innerWidth ||
+document.documentElement.clientWidth;
+console.log([`Die Viewport-Breite beträgt: ${getViewportWidth()} Pixel`]);
+
+//percent -> 30% der Bildschirm-Breite in px 
+//document.documentElement.clientWidth Viewportbreite in px
+
+const percent = window.screen.width * 0.3;
+if(document.documentElement.clientWidth < percent) {
+    console.log(alert("Warnung! Viewport-Breite weniger als 30%"));
+}
